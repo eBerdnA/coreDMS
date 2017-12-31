77cc6fce442c2785b4d2a3c04c1ef953844c9516
Andre Bering <andre@bering.in>
2017-12-31 17:09:15 +0100

    Add project files.

---
+++ coreDMS/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace coreDMS
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
